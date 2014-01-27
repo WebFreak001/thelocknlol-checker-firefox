686851c752c2ae29b56084037fd68e9f8c8cdefe
WebFreak001 <janju007@web.de>
2014-01-27 19:50:45 +0100

    APIs now working

--- Plugin/data/script.js
+++ Plugin/data/script.js
@@ -3,91 +3,22 @@ var refreshRate = 5000, video = "1", livestream = "1", sound = "1", lastVideo = 
 function notify(title, description, link)
 {
 	console.log("[TheLockNLol Checker] Notification: " + title + " " + description);
-	var notification = new Notification(title, { "body": description, "icon": "koala48.png" })
-	
-	notification.onclick = function ()
+	var notification = new Notification(title, { "body": description, "icon": "koala48.png" });
+
+	notification.addEventListener("click", function ()
 	{
 		window.open(link);
 		notification.close();
-	}
-	notification.show();
+	});
 	window.setTimeout(function () { notification.close(); }, 10000);
 }
 
 var isTwitchOnline = false;
 
-function checkTwitch()
-{
-	if (livestream == "1")
-	{
-		if (window.XMLHttpRequest)
-		{
-			xmlhttp = new XMLHttpRequest();
-		}
-		else
-		{
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange = function ()
-		{
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-			{
-				if (xmlhttp.responseText.trim().replace(" ", "") == "[]")
-				{
-					isTwitchOnline = false;
-				} else
-				{
-					if (!isTwitchOnline)
-					{
-						notify("TheLockNLol livestreamt nun!", "TheLockNLol ist grade auf twitch.tv online gekommen! Klick mich um dorthin zu gelangen.", "http://www.twitch.tv/TheLockNLol");
-						if (sound == "1") document.write('<audio id="player" src="sfx.wav" >');
-						document.getElementById('player').play();
-					}
-					isTwitchOnline = true;
-				}
-			}
-		}
-		xmlhttp.open("GET", "http://api.justin.tv/api/stream/list.json?channel=TheLockNLol", false);
-		xmlhttp.send();
-	}
-}
-
-function checkYoutube()
-{
-	if (video == "1")
-	{
-		//http://gdata.youtube.com/feeds/api/users/TheLockNLol/uploads?max-results=10
-		if (window.XMLHttpRequest)
-		{
-			xhttp = new XMLHttpRequest();
-		}
-		else // code for IE5 and IE6
-		{
-			xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xhttp.open("GET", "http://gdata.youtube.com/feeds/api/users/TheLockNLol/uploads?max-results=5", false);
-		xhttp.send();
-		var xml = xhttp.responseXML;
-		var author = xml.getElementsByTagName("author")[0].childNodes[0].nodeValue;
-		var entries = xml.getElementsByTagName("entry");
-		var lastID = "";
-		for (var i = 0; i < entries.length; i++)
-		{
-			var id = entries[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
-			var title = entries[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-			if (id == lastVideo) break;
-			notify("TheLockNLol hat ein neues Video hochgeladen!", title, "http://www.youtube.com/watch?v=" + id.substring(42));
-			if(lastID == "") lastID = id;
-		}
-		if(lastID != "") setValue("lastVideo", lastID);
-	}
-}
-
 function process()
 {
 	console.log("[TheLockNLol Checker] checking...");
-	checkTwitch();
-	checkYoutube();
+	requestExternal();
 }
 
 self.port.on("start-process-interval", function ()
@@ -101,6 +32,48 @@ function setValue(a, v)
 	self.port.emit("set-value", a, v);
 	self.port.emit("get-value", a);
 }
+
+function requestExternal()
+{
+	self.port.emit("ext-pls", "http://api.justin.tv/api/stream/list.json?channel=TheLockNLol");
+	self.port.emit("ext-pls", "http://gdata.youtube.com/feeds/api/users/TheLockNLol/uploads?max-results=5");
+}
+
+self.port.on("receive-ext", function (a, v)
+{
+	if (a == "http://api.justin.tv/api/stream/list.json?channel=TheLockNLol")
+	{
+		if (v.trim().replace(" ", "") == "[]")
+		{
+			isTwitchOnline = false;
+		} else
+		{
+			if (!isTwitchOnline)
+			{
+				notify("TheLockNLol livestreamt nun!", "TheLockNLol ist grade auf twitch.tv online gekommen! Klick mich um dorthin zu gelangen.", "http://www.twitch.tv/TheLockNLol");
+				if (sound == "1") document.write('<audio id="player" src="sfx.wav" >');
+				document.getElementById('player').play();
+			}
+			isTwitchOnline = true;
+		}
+	}
+	if (a == "http://gdata.youtube.com/feeds/api/users/TheLockNLol/uploads?max-results=5")
+	{
+		var parser = new DOMParser();
+		var xml = parser.parseFromString(v, "text/xml");
+		var author = xml.getElementsByTagName("author")[0].childNodes[0].nodeValue;
+		var entries = xml.getElementsByTagName("entry");
+		var lastID = "";
+		for (var i = 0; i < entries.length; i++)
+		{
+			var id = entries[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
+			var title = entries[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+			if (id == lastVideo) break;
+			notify("TheLockNLol hat ein neues Video hochgeladen!", title, "http://www.youtube.com/watch?v=" + id.substring(42));
+			if (lastID == "") lastID = id;
+		}
+	}
+});
 
 self.port.on("give-value", function (a, v)
 {
--- Plugin/lib/main.js
+++ Plugin/lib/main.js
@@ -1,5 +1,7 @@
 var data = require("sdk/self").data;
+var Request = require("sdk/request").Request;
 var ss = require("sdk/simple-storage");
+var chrom = require('chrome');
 var pnl = require("sdk/panel").Panel({
 	width: 370,
 	height: 200,
@@ -31,6 +33,18 @@ var pg = require("sdk/page-mod").PageMod({
 		pageWorker.port.on("set-value", function (a, v)
 		{
 			ss.storage[a] = v;
+		});
+
+		pageWorker.port.on("ext-pls", function (a)
+		{
+			console.log("Requesting " + a);
+			var request = Request({
+				url: a,
+				onComplete: function (response)
+				{
+					pageWorker.port.emit("receive-ext", a, response.text);
+				}
+			}).get();
 		});
 
 		pageWorker.port.emit("start-process-interval");
